652b50e002a668dcf77d6b58ec01899e36cb8598
Felipe Furtado <felipesahb@gmail.com>
2023-11-21 01:35:18 -0300

    parse image to bitecode before sending to OCR

--- tradutor_laudos.py
+++ tradutor_laudos.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import easyocr
+from PIL import Image
+import io
 
-reader = easyocr.Reader(['pt', 'en'], gpu = False)
+reader = easyocr.Reader(['pt', 'en'], gpu=False)
 
 st.header("Tradutor de Laudos de Exames Médicos")
 st.caption("Selecione o arquivo que deseja traduzir")
@@ -9,7 +11,12 @@ st.caption("Selecione o arquivo que deseja traduzir")
 laudo_original = st.file_uploader("Selecione o arquivo", type=['png', 'jpg', 'jpeg'])
 
 if laudo_original is not None:
-    texto_laudo = reader.readtext(laudo_original, detail = 0)
-    # Imprimir texto para debug
+    # Read the uploaded image
+    image = Image.open(io.BytesIO(laudo_original.read()))
+    
+    # Perform OCR on the image
+    texto_laudo = reader.readtext(image, detail=0)
+    
+    # Display the OCR result
     st.header("Texto do Laudo Original")
     st.write(texto_laudo)
