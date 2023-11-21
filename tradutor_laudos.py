03462f3aed6479de5483ed49dfcc818c35c5a675
Felipe Furtado <felipesahb@gmail.com>
2023-11-21 01:49:51 -0300

    add comments for documentation, join list for better output

--- tradutor_laudos.py
+++ tradutor_laudos.py
@@ -3,11 +3,13 @@ import easyocr
 from PIL import Image
 import tempfile
 
+# Initialize EasyOCR with desired languages
 reader = easyocr.Reader(['pt', 'en'], gpu=False)
 
 st.header("Tradutor de Laudos de Exames Médicos")
 st.caption("Selecione o arquivo que deseja traduzir")
 
+# File uploader for image selection
 laudo_original = st.file_uploader("Selecione o arquivo", type=['png', 'jpg', 'jpeg'])
 
 if laudo_original is not None:
@@ -22,8 +24,13 @@ if laudo_original is not None:
     image = Image.open(temp_image.name)
     
     # Perform OCR on the image using EasyOCR
-    texto_laudo = reader.readtext(temp_image.name, detail=0)
+    # Extract text from the temporary image file
+    text_results = reader.readtext(temp_image.name, detail=0)
     
-    # Display the OCR result
+    # Combine the list of strings into a paragraph
+    # Join the list elements with a space
+    texto_laudo = ' '.join(text_results)
+    
+    # Display the OCR result as a paragraph
     st.header("Texto do Laudo Original")
     st.write(texto_laudo)
