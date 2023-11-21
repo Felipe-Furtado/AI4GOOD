27d925afbcd11e6e36d892be85a72bc048840d24
Felipe Furtado <felipesahb@gmail.com>
2023-11-21 00:55:11 -0300

    Commit Inicial

---
+++ tradutor_laudos.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import easyocr
+
+reader = easyocr.Reader(['pt'])
+
+st.heading("Tradutor de Laudos de Exames Médicos")
+st.caption("Selecione o arquivo que deseja traduzir")
+
+laudo_original = st.file_uploader("Selecione o arquivo", type=['png', 'jpg', 'jpeg'])
+
+if laudo_original is not None:
+    texto_laudo = reader.readtext(laudo_original)
+    # Imprimir texto para debug
+    st.header("Texto do Laudo Original")
+    st.write(texto_laudo)
